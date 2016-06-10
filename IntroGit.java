8d3da34c4a4a1283c93dcd22e701f3b6ccd1c74c
Thibaut Emion <thibaut.emion@gmail.com>
2016-06-10 17:47:49 +0200

    Petit programme un peu cool

--- IntroGit.java
+++ IntroGit.java
@@ -1,7 +1,35 @@
-public class IntroGit {
-	
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class IntroGit extends JFrame implements ActionListener {
+
+	JButton b;
+
+	public IntroGit() {
+		setTitle("Fenêtre du swag");
+		setSize(100, 100);
+		setLocationRelativeTo(null);
+
+		b = new JButton("Quitter");
+		b.addActionListener(this);
+
+		add(b);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == b) {
+			JOptionPane.showMessageDialog(this, "Gandalf.", "Gandalf", JOptionPane.NO_OPTION);
+			System.exit(0);
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		new IntroGit();
 	}
 	
 }
